f3152840b0959daf4a08349428b477717f81abf8
dzmitry <dima.lenchevskii@gmail.com>
2021-10-08 18:56:14 +0300

    beautify

--- source/imitation/imitation.js
+++ source/imitation/imitation.js
@@ -6,8 +6,8 @@ const IMITATION_HANDLER = require('./imitation.handler')
  * @param {Function} fnc 
  */
 const mapTCO =
-  (collection, fnc) => IMITATION_HANDLER
-    .mapR(collection, fnc)
+  (collection, fnc) =>
+    IMITATION_HANDLER.mapR(collection, fnc)
 
 /**
  * @param {Array} collection 
@@ -15,10 +15,11 @@ const mapTCO =
  * @param {*} initial 
  */
 const reduceTCO =
-  (collection, fnc, initial) => R.compose(
-    IMITATION_HANDLER.reduceR(R.__, fnc),
-    IMITATION_HANDLER.maybeInitial(initial, R.__)
-  )(collection)
+  (collection, fnc, initial) =>
+    R.compose(
+      IMITATION_HANDLER.reduceR(R.__, fnc),
+      IMITATION_HANDLER.maybeInitial(initial, R.__)
+    )(collection)
 
 module.exports = {
   mapTCO,
